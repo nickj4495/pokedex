93866ec9dce4bf72b194138ecb616fd1907e24ce
nickj4495 <nickj4495@gmail.com>
2020-11-17 20:32:27 -0800

    added function to display images

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+const gallery = document.querySelector(".gallery");
+
+let pokemonImages = [];
+
 async function getData() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/charmander/");
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon/turtonator/"
+    );
+    const data = await response.json();
+    pokemonImages.push(data.sprites.front_default);
+  } catch {
+    console.log(error);
+  }
 }
 
-getData();
+// Show Images
+
+async function displayImages() {
+  await getData();
+  pokemonImages.forEach((item) => {
+    gallery.insertAdjacentHTML(
+      "afterbegin",
+      `
+        <img class='image' src=${item}>
+    `
+    );
+  });
+}
+
+displayImages();
