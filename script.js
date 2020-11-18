8fbb6e8a652663b4566c2b6fc64aca80ca8ba3fb
nickj4495 <nickj4495@gmail.com>
2020-11-17 17:53:56 -0800

    added title and styling, implemented fetch call to pokeapi

---
+++ script.js
@@ -0,0 +1,7 @@
+async function getData() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/charmander/");
+  const data = await response.json();
+  console.log(data);
+}
+
+getData();
