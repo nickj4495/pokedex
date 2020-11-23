17bdd46b2f8a6b95548fe2c4bf0570a02ca7436a
nickj4495 <nickj4495@gmail.com>
2020-11-22 19:11:00 -0800

    created displayPhotos function to render the pokemon images by generation on the browser

--- script.js
+++ script.js
@@ -4,30 +4,72 @@ const gallery = document.querySelector(".gallery");
 
 document.addEventListener("click", (e) => {
   if (!event.target.id) return;
-  // clear our name array before every fetch call
-  generationPokemonNames = [];
+  // clear our pokemon array before every fetch call
+  pokemon = [];
   // fetch call here
-  getGenerations(e.target.id);
+  getData(e.target.id);
 });
 
-let pokemonImages = [];
-let generationPokemonNames = [];
+let pokemon = [];
 
 // Fetch Calls
 
-async function getGenerations(id) {
-  try {
-    const response = await fetch(`https://pokeapi.co/api/v2/generation/${id}/`);
-    const data = await response.json();
-    // create new array of the pokemon names
-    data.pokemon_species.map((item) => {
-      // we only want the name property of each object in the array
-      if (Object.getOwnPropertyNames(item).includes("name")) {
-        generationPokemonNames.push(`${item.name}`);
-      }
-    });
-  } catch {
-    console.log(error);
-  }
-  console.log(generationPokemonNames);
+async function getData(id) {
+  // retrieve list of pokemon names based on generation id
+  const generation = await fetch(
+    `https://pokeapi.co/api/v2/generation/${id}/`
+  ).then((response) => response.json());
+  // create an array of objects for each pokemon in the generation
+  generation.pokemon_species.map(async (item) => {
+    const pokeId = await getId(item.url);
+    pokemon.push({ name: item.name, id: pokeId });
+    displayImages();
+  });
 }
+
+async function getId(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.id;
+}
+
+function displayImages() {
+  clearGallery();
+  pokemon.forEach(({ name, id }) => {
+    // console.log(name, imgUrl);
+    gallery.insertAdjacentHTML(
+      "beforeend",
+      `
+            <div class="img-container">
+                <div class="title">${name}</div>
+                <img class="img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" alt='image of ${name}'/>
+            </div>
+            `
+    );
+  });
+}
+
+function clearGallery() {
+  gallery.innerHTML = "";
+}
+
+// TESTING
+
+// async function getPokemon() {
+//   const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+//   const data = await response.json();
+//   console.log(data.results[0].url);
+//   data.results.forEach(async (item) => {
+//     const response1 = await fetch(item.url);
+//     const data1 = await response1.json();
+//     console.log(data1);
+//   });
+// }
+
+// async function yay() {
+//   const response = await fetch("https://pokeapi.co/api/v2/pokemon-species/1/");
+//   const data = await response.json();
+//   console.log(data);
+// }
+
+// yay();
