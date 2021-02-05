03e0f515b5e3b9b625b8a5fa23e417a549a1dd12
Running <running@rw-op-05.local>
2021-02-05 12:04:27 -0800

    added animejs script found online for animated text at top of page

--- script.js
+++ script.js
@@ -201,3 +201,39 @@ function displayImages(arr) {
   filter.scrollIntoView({behavior: "smooth", block: "start"});
   clearLoader();
 }
+
+
+//////// ANIMATION FROM https://tobiasahlin.com/moving-letters/
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 700,
+    easing: "easeOutExpo",
+    delay: 700
+  });
